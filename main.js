9f1d652edd444c1ee94a209d96e606265822f449
Prabakaransr19 <prabakaransr198@gmail.com>
2025-01-20 00:27:06 +0530

    API integreted

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 let cityInput = document.getElementById('city-input');
 let searchBtn = document.getElementById('searchbtn');
+let locationBtn = document.getElementById('locationbtn');
 let api_key = 'db79a9b6323b7c4a29ed5d50735feff8',
 currentWeatherCard = document.querySelectorAll('.weather-left .card')[0],
-fiveDaysForecastCard = document.querySelector('.day-forcast');
+fiveDaysForecastCard = document.querySelector('.day-forcast'),
+aqiCard = document.querySelectorAll('.highlights .card')[0],
+sunriseCard = document.querySelectorAll('.highlights .card')[1],
+
+humidityVal = document.querySelector('#humidityValue'),
+pressureVal = document.querySelector('#pressureValue'),
+visibilityVal = document.querySelector('#visibilityValue'),
+windspeedVal = document.querySelector('#windspeedValue'),
+
+aqilist = ['Good','Fair','Moderate','Poor','Very Poor'];
 
 function getWeatherDetails(name,lat,lon,country,state){
     let FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`,
     WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`,
+    AIR_POLLUTION_API_URL = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${api_key}`,
     days = [
         'Sunday',
         'Monday',
@@ -19,10 +30,57 @@ function getWeatherDetails(name,lat,lon,country,state){
 
     months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
+    fetch(AIR_POLLUTION_API_URL).then(res=>res.json()).then(data=>{
+        let {co,no,no2,o3,so2,pm2_5,pm10,nh3} = data.list[0].components;
+        aqiCard.innerHTML = `
+        <div class="card-head">
+                <p>Air Quality Index</p>
+                <p class="air-index aqi-${data.list[0].main.aqi}">${aqilist[data.list[0].main.aqi - 1]}</p>
+            </div>
+            <div class="air-indices">
+                <i class="fa-regular fa-wind fa-3x"></i>
+                <div class="item">
+                    <p>PM2.5</p>
+                    <h2>${pm2_5}</h2>
+                </div>
+                <div class="item">
+                    <p>PM10</p>
+                    <h2>${pm10}</h2>
+                </div> 
+                <div class="item">
+                    <p>SO2</p>
+                    <h2>${so2}</h2>
+                </div>                           
+                <div class="item">
+                    <p>CO</p>
+                    <h2>${co}</h2>
+                </div>
+                <div class="item">
+                    <p>NO</p>
+                    <h2>${no}</h2>
+                </div>
+                <div class="item">
+                    <p>NO2</p>
+                    <h2>${no2}</h2>
+                </div>
+                <div class="item">
+                    <p>NH3</p>
+                    <h2>${nh3}</h2>
+                </div>
+                <div class="item">
+                    <p>O3</p>
+                    <h2>${o3}</h2>
+                </div>
+            </div>`
+    }).catch(()=>{
+        alert('Air pollution data fetch failed');
+    });
+
+
     fetch(WEATHER_API_URL).then(res=>res.json()).then(data=>{
         let date = new Date();
         currentWeatherCard.innerHTML = `
-                <div class="current-weather">
+        <div class="current-weather">
             <div class="details">
                 <p>Now</p>
                 <h2>${(data.main.temp - 273.15).toFixed(2)}&deg;c</h2>
@@ -37,8 +95,43 @@ function getWeatherDetails(name,lat,lon,country,state){
             <p><i class="fa-light fa-location-dot"></i> ${name},${country}</p>
         </div>
         `;
+        let{sunrise,sunset} = data.sys,
+        {timezone,visibility} = data,
+        {humidity,pressure} = data.main,
+        {speed} = data.wind,
+        sRiseTime = moment.utc(sunrise, 'X').add(timezone,'seconds').format('hh:mm A'),
+        sSetTime = moment.utc(sunset, 'X').add(timezone,'seconds').format('hh:mm A');
+        sunriseCard.innerHTML = `
+        <div class="card-head">
+            <p>Sunrise & Sunset</p>
+        </div>
+        <div class="sunrise-sunset">
+            <div class="item">
+                <div class="icon">
+                    <i class="fa-light fa-sunrise fa-4x"></i>
+                </div>
+                <div>
+                    <p>Sunrise</p>
+                    <h2>${sRiseTime}</h2>
+                </div>
+            </div>
+            <div class="item">
+                <div class="icon">
+                    <i class="fa-light fa-sunset fa-4x"></i>
+                </div>
+                <div>
+                    <p>Sunset</p>
+                    <h2>${sSetTime}</h2>
+                </div>
+            </div>
+        `;
+        humidityVal.innerHTML = `${humidity}%`;
+        pressureVal.innerHTML = `${pressure}hPa`;
+        visibilityVal.innerHTML = `${visibility/1000}km`;
+        windspeedVal.innerHTML = `${speed}m/s`;
+
     }).catch(()=>{
-        alert('Failed to fetch current data')
+        alert('Failed to fetch current data');
     });
 
     fetch(FORECAST_API_URL).then(res=>res.json()).then(data=>{
@@ -82,3 +175,25 @@ function getCityCoordinates(){
 }
 
 searchBtn.addEventListener('click',getCityCoordinates);
+locationBtn.addEventListener('click',getuserCoordinates);
+
+function getuserCoordinates(){
+    navigator.geolocation.getCurrentPosition(position=>{
+        let{latitude,longitude} = position.coords;
+        let REVERSE_GEOCODING_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${api_key}`;
+
+        fetch(REVERSE_GEOCODING_URL).then(res=>res.json()).then(data=>{
+            let {name,country,state} = data[0];
+            getWeatherDetails(name,latitude,longitude,country,state);
+        }).catch(()=>{
+            alert('Cant find current location');
+        });
+
+    },error=>{
+        if(error.code === error.PERMISSION_DENIED){
+            alert("Allow location to access");
+        }
+    }
+);
+}
+
